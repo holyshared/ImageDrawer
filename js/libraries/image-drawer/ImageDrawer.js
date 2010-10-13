25902e75f90ab5ea08d09fa3324aa20e70737678
Noritaka Horio <holy.shared.design@gmail.com>
2010-10-13 21:40:01 +0900

    The header is corrected.
    Signed-off-by: Noritaka Horio <holy.shared.design@gmail.com>

--- js/libraries/image-drawer/ImageDrawer.js
+++ js/libraries/image-drawer/ImageDrawer.js
@@ -10,27 +10,24 @@ authors:
 - Noritaka Horio
 
 requires:
-  core/1.2.4:
   - Core/Core
+  - Core/Array
+  - Core/String
+  - Core/Number
+  - Core/Function
+  - Core/Object
+  - Core/Event
   - Core/Browser
-  - Native/Array
-  - Native/Function
-  - Native/Number
-  - Native/String
-  - Native/Hash
-  - Native/Event
-  - Class/Class
-  - Class/Class.Extras
-  - Element/Element
-  - Element/Element.Event
-  - Element/Element.Style
-  - Element/Element.Dimensions
-  - Utilities/Selecter
-  - Utilities/DomReady
-  - Fx/Fx
-  - Fx/Fx.Transitions
+  - Core/Class
+  - Core/Class.Extras
+  - Core/Element
+  - Core/Element.Style
+  - Core/Element.Event
+  - Core/Element.Dimensions
+  - Core/Fx
+  - Core/Fx.Transitions
 
-provides: [ImageDrawer, ImageDrawer.Grid, ImageDrawer.Expand, Fx.ImageDrawer]
+provides: [Fx.ImageDrawer, ImageDrawer]
 
 ...
 */
